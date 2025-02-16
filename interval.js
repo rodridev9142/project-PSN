09e5b6bca3dc8961d7165d1b53a704e628e9917e
rodridev9142 <jessicajulia9142@gmail.com>
2025-02-16 10:17:52 -0300

    Update interval.js

--- interval.js
+++ interval.js
@@ -1,8 +1,5 @@
 //import { foto } from "./cart-added-js";
 
-export let addImagens = [
-  "https://assets.propmark.com.br/uploads/2020/10/Ali1-FASHION-1200x628-1.jpg",
-  "https://i2.wp.com/assets.propmark.com.br/uploads/2020/10/Ali2-TECH-1200x628-1-1024x536.jpg?resize=720%2C377&ssl=1",
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3qn2sVrSACw7fNfk7tYjAjphMLwBMqp7-gv32KT3VBgaQtY3cae9qbMQfcXqZCLu_9o4&usqp=CAU",
+export let addImagens = ['https://assets.propmark.com.br/uploads/2020/10/Ali1-FASHION-1200x628-1.jpg', 'https://i.ytimg.com/vi/evhpwteggA8/maxresdefault.jpg', 'https://i.ytimg.com/vi/-ggE3y918HU/hq720.jpg?sqp=-oaymwE7CK4FEIIDSFryq4qpAy0IARUAAAAAGAElAADIQj0AgKJD8AEB-AH-DoACuAiKAgwIABABGH8gQSgxMA8=&rs=AOn4CLC_RcjUCyNSkuHwWEUfLtnq_j9kDg','https://www.promobit.com.br/blog/wp-content/uploads/2022/06/24141522/aliexpress.jpg',
 ]
 
