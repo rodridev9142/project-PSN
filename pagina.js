1a71e181a4007e0dff725cbe2a517dd571238fee
rodridev9142 <jessicajulia9142@gmail.com>
2025-04-06 10:43:08 -0300

    Update pagina.js

--- pagina.js
+++ pagina.js
@@ -35,7 +35,7 @@ document.querySelector('.last-news')
 function plus() {
   let mais = 0;
   document.querySelector('.mais')
-    .addaddEventListener('click',( ) {
+    .addaddEventListener('click',( ) => {
       mais ++;
       document.querySelector('.teste')
         .innerHTML = mais;
