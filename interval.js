03ee4820cf3ac02fa5bbeeb75aa72262a67fc45e
rodridev9142 <jessicajulia9142@gmail.com>
2025-11-14 12:40:02 -0300

    Delete interval.js

--- interval.js
+++
@@ -1,5 +0,0 @@
-//import { foto } from "./cart-added-js";
-
-export let addImagens = ['https://assets.propmark.com.br/uploads/2020/10/Ali1-FASHION-1200x628-1.jpg', 'https://i.ytimg.com/vi/evhpwteggA8/maxresdefault.jpg', 'https://i.ytimg.com/vi/-ggE3y918HU/hq720.jpg?sqp=-oaymwE7CK4FEIIDSFryq4qpAy0IARUAAAAAGAElAADIQj0AgKJD8AEB-AH-DoACuAiKAgwIABABGH8gQSgxMA8=&rs=AOn4CLC_RcjUCyNSkuHwWEUfLtnq_j9kDg','https://www.promobit.com.br/blog/wp-content/uploads/2022/06/24141522/aliexpress.jpg',
-]
-
